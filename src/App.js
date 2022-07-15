b752ec17b08238c6cea3195bda2ce1647075e75e
Strevino2 <strevino2@hotmail.com>
2022-07-14 21:05:28 -0500

    hosting 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Button } from "./components/Button";
-import { ButtonClass } from "./components/ButtonClass";
+// import { ButtonClass } from "./components/ButtonClass";
 import "./App.css";
 
 function App() {
@@ -16,9 +16,9 @@ function App() {
       <Button label="Submit" handleClick={handleSubmit} />
       <Button label="Clear" handleClick={handleClear} />
       <Button />
-      <ButtonClass label="Submit Class" handleClick={handleSubmit} />
+      {/* <ButtonClass label="Submit Class" handleClick={handleSubmit} />
       <ButtonClass label="Clear Class" handleClick={handleClear} />
-      <ButtonClass />
+      <ButtonClass /> */}
     </div>
   );
 }
--- src/components/ButtonClass.js
+++ src/components/ButtonClass.js
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-export class ButtonClass extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+// export class ButtonClass extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
 
-  render() {
-    const {
-      label = "Default",
-      handleClick = () => console.log("I am default"),
-    } = this.props;
-    return <button onClick={handleClick}>{label}</button>;
-  }
-}
+//   render() {
+//     const {
+//       label = "Default",
+//       handleClick = () => console.log("I am default"),
+//     } = this.props;
+//     return <button onClick={handleClick}>{label}</button>;
+//   }
+// }
